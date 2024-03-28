91ae2a039dad541a52fff4e5b8f93c64ebf2c175
itmesarah <itmesarah@mail.com>
2024-03-28 15:43:06 -0500

    Update discordclasses.js

--- discordclasses.js
+++ discordclasses.js
@@ -5019,9 +5019,12 @@
   597338: function(e) {
     "use strict";
     e.exports = {
+      addText: "addText__8f18c",
+      clickable: "clickable_a588cd",
       contentLabel: "contentLabel_aa05d5",
       contentWithMinHeight: "contentWithMinHeight__78ad4",
       gameImagePlaceholder: "gameImagePlaceholder__732e7",
+      icon: "icon_e8e1b2",
       inputContainer: "inputContainer__1e798",
       playstyleOption: "playstyleOption__67567",
       playstyleSelect: "playstyleSelect_b7d88b",
@@ -5032,6 +5035,8 @@
       slideContent: "slideContent__6ae94",
       subtitle: "subtitle__62596",
       tagInput: "tagInput__4fbf8",
+      timeSelection: "timeSelection_cf3393",
+      timeSelectorRow: "timeSelectorRow_d86011",
       title: "title__20916"
     }
   },
@@ -7191,6 +7196,14 @@
       header: "header__88d0c",
       icon: "icon__37393",
       iconWrapper: "iconWrapper_a8f659"
+    }
+  },
+  591373: function(e) {
+    "use strict";
+    e.exports = {
+      buttons: "buttons__15cf3",
+      container: "container__8aa3d",
+      formContainer: "formContainer_f90333"
     }
   },
   996650: function(e) {
@@ -21258,6 +21271,12 @@
       emojiIcon: "emojiIcon_c92c67",
       infoContainer: "infoContainer__74c31",
       name: "name_d4dd76"
+    }
+  },
+  320757: function(e) {
+    "use strict";
+    e.exports = {
+      storeBtn: "storeBtn_aa1533"
     }
   },
   886190: function(e) {
