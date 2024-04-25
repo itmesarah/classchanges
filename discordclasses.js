f8841a0d7a2bba099778b5eca5eaf66b094bcd7e
itmesarah <itmesarah@mail.com>
2024-04-24 20:57:39 -0500

    Update discordclasses.js

--- discordclasses.js
+++ discordclasses.js
@@ -20241,6 +20241,7 @@
             newBadge: "newBadge__905d8",
             nitroWheel: "nitroWheel__28082",
             playButton: "playButton_a7f035",
+            text: "text__3eeb0",
             tooltip: "tooltip_b4086a"
         }
     },
