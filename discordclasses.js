80de72fa05b28b5d8dcf68de6921f10f036d4b9b
itmesarah <itmesarah@mail.com>
2024-05-06 19:34:35 -0500

    Update discordclasses.js

--- discordclasses.js
+++ discordclasses.js
@@ -19998,8 +19998,10 @@
             container: "container_e8183e",
             description: "description_c2c202",
             embed: "embed__26f68",
-            emoji: "emoji__496e3",
-            title: "title_eb3605"
+            imageContainer: "imageContainer_b8eb37",
+            victorAnswer: "victorAnswer__797b7",
+            victorAnswerText: "victorAnswerText_d5541e",
+            victorIcon: "victorIcon_b173ae"
         }
     },
     957036: function(e) {
