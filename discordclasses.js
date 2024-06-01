aeaf68db3cac94be788a4ca44556919972e83d56
itmesarah <itmesarah@mail.com>
2024-06-01 18:19:20 -0500

    Update discordclasses.js

--- discordclasses.js
+++ discordclasses.js
@@ -18824,7 +18824,6 @@
     897456: function(e) {
         "use strict";
         e.exports = {
-            footer: "footer_d9904f",
             oauth2Wrapper: "oauth2Wrapper__5c685",
             wave: "wave__06e47"
         }
